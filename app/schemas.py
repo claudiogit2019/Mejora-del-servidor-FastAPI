94acea49bab8e857b48aa294d46c6cb24b823d84
claudiogit2019 <claudiounju2012@gmail.com>
2024-06-13 17:41:50 -0300

    Añadir proyecto desde la carpeta tut3

---
+++ app/schemas.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel
+
+class ItemBase(BaseModel):
+    title: str
+    description: str | None = None
+    price: float
+
+class ItemCreate(ItemBase):
+    pass
+
+class Item(ItemBase):
+    id: int
+
